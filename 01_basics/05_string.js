d963761d2c2047388ffc197ab30d996be9aaa6a2
ayash2004 <103133275+ayash2004@users.noreply.github.com>
2025-05-11 13:36:51 +0000

    string Operations

---
+++ 01_basics/05_string.js
@@ -0,0 +1,53 @@
+// const string = " Yash "
+// const altstring = " Akash "
+
+// const age = "22"
+// console.log(string + altstring + "Hello World")
+// console.log(`Hello my name is ${string} and my Age is ${age}`);
+
+// const string = " Yash "
+// console.log(string);
+// console.log(typeof(string));
+
+// const website = new String('yash-agarwal-com')  //new string is an considered an Object
+// console.log(website);  //Output: [String: 'yash-agarwal-com']
+// console.log(typeof(website)); //Output: Object
+
+// const gamename = "Gold star Yash"
+// const split = gamename.split(" ");
+// console.log(gamename.split(" "));
+// console.log(split[0]);
+// console.log(split[1]);
+// console.log(split[2]);
+
+gameName = "Gold Star Yash"
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(5, 9) // Includes startIndex and Excludes EndIndex
+// console.log(anotherString);
+
+// const newStringOne = "   Yash    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim()); //Removes White Spaces from Start and End 
+
+// console.log(gamename.);
+// console.log(gamename.);
+// console.log(gamename.);
+// console.log(gamename.);
+
+
+// console.log(gamename.charAt(3));
+// console.log(gamename.toUpperCase());
+// console.log(gamename.length);
+// console.log(gamename.indexOf('d'));
+
+const url = "https://yash.com/yash%20agarwal"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split(' '));
+
+
