b68f2dbb213a802c21655d68912db28a4f3a9b96
ayash2004 <103133275+ayash2004@users.noreply.github.com>
2025-06-09 05:02:14 +0000

    class and function

---
+++ 10_classes_and_oop/myclass.js
@@ -0,0 +1,36 @@
+
+// class User {
+//     constructor(username,email, password){
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+//     }
+
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+//     changeUsername(){
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+
+// const chai = new User ("Yash","Yash@FB.com", "123")
+// console.log(chai)
+// console.log(chai.encryptPassword())
+// console.log(chai.changeUsername())
+
+function User (username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password;
+}
+
+User.prototype.encryptPassword = function () {
+    return `${this.password}abc`}
+User.prototype.changeUsername = function() {
+    return `${this.username.toUpperCase()}`}
+    
+const tea = new User ("Tea", "tea@email.com", "456")
+console.log(tea)
+console.log(tea.encryptPassword())
+console.log(tea.changeUsername())
